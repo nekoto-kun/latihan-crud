a985bcc0c06b67d54d58f188eaeb119f4c2943d4
Andre Kho <andreyonathan65@gmail.com>
2022-05-12 09:51:08 +0700

    Add dynamic title

--- public/view.php
+++ public/view.php
@@ -15,7 +15,7 @@ $course = CourseController::getOneCourse($_GET['id']);
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-  <title>Home</title>
+  <title><?= $course['title'] ?></title>
 
   <style>
     .cover {
